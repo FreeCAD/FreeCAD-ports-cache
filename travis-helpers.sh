d4e172ee91240dce52d2fcb3cf1a8fa8336a900e
Bruce Lacey <bruce@blacey.com>
2018-12-16 08:30:01 -0500

    Purging existing cache using `mv` instead of `rm` to avoid permission issues

--- travis-helpers.sh
+++ travis-helpers.sh
@@ -66,7 +66,8 @@ ports_archive_filename()
 purge_local_ports_cache()
 {
    portsRoot=${1:-/usr/local}
-   sudo find  ${portsRoot} -maxdepth 1 ! -path ${portsRoot} ! -path . -print0 | xargs -0 rm -rf
+   # Removing /usr/local directories results in errors so just move aside
+   mkdir ~/.usr_local && sudo mv ${portsRoot}/* ~/.usr_local
    return $?
 }
 
